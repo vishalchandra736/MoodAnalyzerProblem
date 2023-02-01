c98173ac849b8823aec08642ac05975d7017c578
Vishal Chandra <109501380+vishalchandra736@users.noreply.github.com>
2023-02-01 18:40:47 +0530

    Added : Mood Analyzer Problem

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,10 +1,25 @@
 package com.blz.moodanalyzer;
 
 public class MoodAnalyzer {
+    private String message;
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+    public String analyseMood() {
+        return analyseMood(this.message);
+    }
+
     public String analyseMood(String message) {
-        if(message.contains("Sad"))
-            return "SAD";
-        else
+        try {
+            if(message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
