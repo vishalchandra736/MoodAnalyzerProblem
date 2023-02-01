a075843a1e43fc343cc21c519da0d99eb36c7e25
Vishal Chandra <109501380+vishalchandra736@users.noreply.github.com>
2023-02-01 20:09:50 +0530

    Added : Mood Analyzer Problem

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,10 +1,31 @@
 package com.blz.moodanalyzer;
-
 public class MoodAnalyzer {
-    public String analyseMood(String message) {
-        if(message.contains("Sad"))
-            return "SAD";
-        else
-            return "HAPPY";
+    String message = null;
+    public MoodAnalyzer() {
+    }
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    /**
+     * use for analyse mood
+     * @param message
+     * @return String: if msg is Sad return SAD mood; otherwise return HAPPY mood
+     */
+    public String analyseMood(String message) throws MoodAnalyzerException{
+        try{
+            if(message.contains("Sad")) {
+                return "SAD";
+            }
+        }
+        catch(NullPointerException ex) {
+            if(message == null){
+                throw new MoodAnalyzerException("Invalid Input!!!");
+            }
+        }
+        return "HAPPY";
+    }
+    public String analyseMood() {
+        return analyseMood(this.message);
     }
 }
